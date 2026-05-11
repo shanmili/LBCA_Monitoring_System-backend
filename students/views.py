c97ac36acfb386b238fb7b61fc8657142f7d3565
shanmilidgp <degala.shanmaeleigh@gmail.com>
2026-05-12 00:09:55 +0800

    AppDev: DRF backend for system implemented

--- students/views.py
+++ students/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from .models import Student, StudentEnrollment
